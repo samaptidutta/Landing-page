07ed41dd8bd65db7adba59491870a41b45b99fd4
samaptidutta <duttasamapti24@gmail.com>
2025-04-17 13:44:46 +0530

    my landing page

--- Mylandingpage/src/App.jsx
+++ Mylandingpage/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Features from './components/Features'
 import Herosection from './components/Herosection'
+import Myfooter from './components/Myfooter'
 import Navbar from './components/Navbar'
 import PricingSystem from './components/PricingSystem'
 import Testimonials from './components/Testimonials'
@@ -16,11 +17,12 @@ function App() {
       <Navbar/>
 
     <div className='max-w-7xl mx-auto pt-20 px-6'>
-    <Herosection/>
-    <Features/>
-    <Workflow/>
-    <PricingSystem/>
-    <Testimonials/>
+      <Herosection/>
+      <Features/>
+      <Workflow/>
+      <PricingSystem/>
+      <Testimonials/>
+      <Myfooter/>
     </div>
 
       
